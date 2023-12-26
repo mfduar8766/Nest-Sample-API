fbcda126880c365d144d5bfe75384c1dad2770da
Matheus Duarte <mfduar8766@gmail.com>
2023-12-26 14:58:36 -0500

    'changing'

--- gitAddCommitPush.sh
+++ gitAddCommitPush.sh
@@ -2,48 +2,31 @@
 
 function addcommitpush () {
 
-echo "Adding and commiting files"
+echo "Adding and commiting files..."  &
+
+REGEX='^(feature|bugfix|refactor)\/[A-Za-z].*$'
+
+message=\'"$@"\'
 
 current=$(git branch | grep "*" | cut -b 3-)
 
-message=\'"$@"\'
-git add --all :/ && git commit -a -m "$message"
+echo $current
 
-# feature/debug/refactor options for 1st part of branch name
-echo "Select branch type and enter a branch name:
-f - feature
-d - debug
-r - refactor"
-
-read feature
-case $feature in
-    f) #feature
-    feature="FEATURE"
-    ;;
-    d) #debug
-    feature="DEBUG"
-    ;;
-    r) #refactor
-    feature="REFACTOR"
-    ;;
-    *) #Invalid option
-    echo "Invalid Option"
-    feature=""
-    ;;
-esac
-
-echo "Where to push?"
-read -i "$feature/$current" -e branch
-
-git checkout -b "$feature/$current"
+if [[ $current =~ $REGEX ]]; then
+  git add --all :/ && git commit -a -m "$message"
+else
+  echo "Branch name must be prefixed with feature/, refactor/, bugfix/..."
+  exit 1
+fi
 
 echo "You sure you wanna push? (y/n)"
-read -i "y" -e yn
+read yn
 
-if [ "$yn" = y ]; then
-  git push origin "$branch"
+if [ $yn = y ]; then
+  git push origin $current
 else
-  echo "Have a nice day!"
+  echo "Could not push to branch: $current ..."
+  exit 1
 fi
 }
 addcommitpush $1
