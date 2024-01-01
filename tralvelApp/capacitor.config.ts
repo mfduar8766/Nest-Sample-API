8de4c977531bc65a5525b9e87a8353ee8eed1492
Matheus Duarte <mfduar8766@gmail.com>
2024-01-01 16:22:29 -0500

    'added vueJS mobile app boiler plate'

---
+++ tralvelApp/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tralvelApp',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
