dc97c0c5785f4a848a5cd304b88ac2cf336b99d0
Matt <30513439+mfduar8766@users.noreply.github.com>
2024-01-04 08:24:42 -0500

    'added vueJS mobile app boiler plate' (#30)

---
+++ tralvelApp/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <ion-app>
+    <ion-content>
+      <ion-router-outlet />
+    </ion-content>
+  </ion-app>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ tralvelApp/src/views/LogInSignUp/LogInSignUp.vue
@@ -0,0 +1,22 @@
+<template>
+  <ion-page>
+    <ion-content>
+      AUTH
+    </ion-content>
+    <ion-button @click="goToTabs" >Go to Tabs</ion-button>
+  </ion-page>
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const goToTabs = () => {
+  router.push({ path: '/tabs/' });
+};
+
+</script>
+
+<style scoped>
+</style>
